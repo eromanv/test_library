6b6985fb31f6c41382822ca65ee98066fc49aab0
Roman Yegorov <e.roman.v@ya.ru>
2024-03-10 16:56:46 +0300

    added database, migrations

--- library/communication/models.py
+++ library/communication/models.py
@@ -1,3 +1,27 @@
+# myapp/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Organization(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organizations = models.ManyToManyField(Organization, related_name="events")
+    image = models.ImageField(upload_to="event_images/", null=True, blank=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- library/library/settings.py
+++ library/library/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+!11usza_uj75^itv1m^ivzo!^t#&82kl^zprdwu@tgo8b48n'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
